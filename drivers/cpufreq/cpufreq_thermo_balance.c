1048a37726181c05492ef51c5ca222094bb09be0
Jayzee-Zee <i.am.hipop@gmail.com>
2025-05-23 21:04:06 +0800

    Update cpufreq_thermo_balance.c

--- drivers/cpufreq/cpufreq_thermo_balance.c
+++ drivers/cpufreq/cpufreq_thermo_balance.c
@@ -1,128 +1,142 @@
 // SPDX-License-Identifier: GPL-2.0
-/*
- * Thermo Balance Governor - Based on Interactive Governor Logic
- * Focus: Lower heat, preserve battery, keep UI smooth
- */
-
-#include <linux/cpufreq.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/cpufreq.h>
+#include <linux/init.h>
+#include <linux/input.h>
+#include <linux/power_supply.h>
+#include <linux/jiffies.h>
 #include <linux/timer.h>
 #include <linux/workqueue.h>
 #include <linux/slab.h>
-#include <linux/mutex.h>
-#include <linux/notifier.h>
-#include <linux/suspend.h>
-#include <linux/input.h>
 
-#define BOOST_FREQ_STEP 200000
-#define FREQ_DROP_STEP 100000
-#define IDLE_TIMEOUT_MS 250
-#define BOOST_TIMEOUT_MS 1500
-#define LOW_BATTERY_THRESHOLD 20
+#define GOVERNOR_NAME "thermo_balance"
 
-struct thermo_cpuinfo {
-	struct cpufreq_policy *policy;
-	struct timer_list idle_timer;
-	struct timer_list boost_timer;
-	struct mutex lock;
-	unsigned int target_freq;
-	bool boosted;
-	bool screen_on;
+static struct cpufreq_policy *global_policy;
+static struct input_handler thermo_input_handler;
+static struct timer_list thermo_timer;
+static struct work_struct thermo_work;
+
+static unsigned int touch_boost_freq = 0;
+static unsigned int idle_freq = 0;
+static unsigned int battery_threshold = 20;
+static bool on_battery = true;
+
+static void thermo_update_freq(struct work_struct *work)
+{
+	unsigned int target_freq = idle_freq;
+
+	if (touch_boost_freq && on_battery) {
+		target_freq = touch_boost_freq;
+	}
+
+	if (global_policy)
+		__cpufreq_driver_target(global_policy, target_freq, CPUFREQ_RELATION_H);
+}
+
+static void thermo_timer_callback(struct timer_list *t)
+{
+	schedule_work(&thermo_work);
+	mod_timer(&thermo_timer, jiffies + msecs_to_jiffies(500));
+}
+
+static void thermo_input_event(struct input_handle *handle,
+                               unsigned int type,
+                               unsigned int code,
+                               int value)
+{
+	if (type == EV_ABS || type == EV_KEY)
+		schedule_work(&thermo_work);
+}
+
+static int thermo_power_event(struct notifier_block *nb,
+                              unsigned long event,
+                              void *data)
+{
+	struct power_supply *psy = data;
+	union power_supply_propval val;
+
+	if (!psy || !psy->get_property)
+		return NOTIFY_OK;
+
+	if (psy->get_property(psy, POWER_SUPPLY_PROP_PRESENT, &val))
+		return NOTIFY_OK;
+
+	on_battery = !val.intval;
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block thermo_psy_notifier = {
+	.notifier_call = thermo_power_event,
 };
 
-static struct thermo_cpuinfo *g_info;
-static int battery_level = 100;
-static bool is_charging = false;
-
-static void apply_freq(struct thermo_cpuinfo *info, unsigned int freq)
+static int thermo_input_connect(struct input_handler *handler,
+                                struct input_dev *dev,
+                                const struct input_device_id *id)
 {
-	if (!info || !info->policy)
-		return;
+	struct input_handle *handle;
+	int error;
 
-	freq = clamp(freq, info->policy->min, info->policy->max);
-	cpufreq_driver_target(info->policy, freq, CPUFREQ_RELATION_L);
-	info->target_freq = freq;
-}
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
 
-static void boost_timeout(struct timer_list *t)
-{
-	struct thermo_cpuinfo *info = from_timer(info, t, boost_timer);
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = "thermo_input_handle";
 
-	mutex_lock(&info->lock);
-	if (info->boosted) {
-		info->boosted = false;
-		apply_freq(info, info->policy->min + FREQ_DROP_STEP);
-	}
-	mutex_unlock(&info->lock);
-}
+	error = input_register_handle(handle);
+	if (error)
+		goto err_free;
 
-static void idle_handler(struct timer_list *t)
-{
-	struct thermo_cpuinfo *info = from_timer(info, t, idle_timer);
+	error = input_open_device(handle);
+	if (error)
+		goto err_unregister;
 
-	mutex_lock(&info->lock);
-	if (!info->boosted) {
-		unsigned int drop = FREQ_DROP_STEP;
-
-		if (!is_charging && battery_level <= LOW_BATTERY_THRESHOLD)
-			drop += 50000; // Save more battery when low
-
-		if (!info->screen_on)
-			drop += 100000;
-
-		if (info->target_freq > info->policy->min + drop)
-			apply_freq(info, info->target_freq - drop);
-	}
-	mod_timer(&info->idle_timer, jiffies + msecs_to_jiffies(IDLE_TIMEOUT_MS));
-	mutex_unlock(&info->lock);
-}
-
-static int thermo_input_event(struct input_handle *handle, unsigned int type,
-                              unsigned int code, int value)
-{
-	if (type == EV_KEY || type == EV_ABS || type == EV_REL) {
-		struct thermo_cpuinfo *info = g_info;
-
-		mutex_lock(&info->lock);
-		if (!info->boosted) {
-			unsigned int new_freq = info->policy->max;
-			apply_freq(info, new_freq);
-			info->boosted = true;
-			mod_timer(&info->boost_timer, jiffies + msecs_to_jiffies(BOOST_TIMEOUT_MS));
-		}
-		mutex_unlock(&info->lock);
-	}
 	return 0;
+
+err_unregister:
+	input_unregister_handle(handle);
+err_free:
+	kfree(handle);
+	return error;
 }
 
-static struct input_handle thermo_input_handle;
+static void thermo_input_disconnect(struct input_handle *handle)
+{
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id thermo_ids[] = {
+	{ .driver_info = 1 }, /* Match all devices */
+	{ },
+};
+
 static struct input_handler thermo_input_handler = {
 	.event = thermo_input_event,
+	.connect = thermo_input_connect,
+	.disconnect = thermo_input_disconnect,
 	.name = "thermo_input_handler",
-	.id_table = NULL,
+	.id_table = thermo_ids,
 };
 
 static int thermo_start(struct cpufreq_policy *policy)
 {
-	struct thermo_cpuinfo *info;
+	global_policy = policy;
 
-	info = kzalloc(sizeof(*info), GFP_KERNEL);
-	if (!info)
-		return -ENOMEM;
+	touch_boost_freq = policy->max;
+	idle_freq = policy->min;
 
-	info->policy = policy;
-	info->target_freq = policy->min + FREQ_DROP_STEP;
-	info->boosted = false;
-	info->screen_on = true;
-	mutex_init(&info->lock);
+	timer_setup(&thermo_timer, thermo_timer_callback, 0);
+	mod_timer(&thermo_timer, jiffies + msecs_to_jiffies(500));
 
-	timer_setup(&info->idle_timer, idle_handler, 0);
-	timer_setup(&info->boost_timer, boost_timeout, 0);
+	INIT_WORK(&thermo_work, thermo_update_freq);
+	schedule_work(&thermo_work);
 
-	mod_timer(&info->idle_timer, jiffies + msecs_to_jiffies(IDLE_TIMEOUT_MS));
-
-	g_info = info;
-
+	power_supply_reg_notifier(&thermo_psy_notifier);
 	input_register_handler(&thermo_input_handler);
 
 	return 0;
@@ -130,34 +144,33 @@ static int thermo_start(struct cpufreq_policy *policy)
 
 static void thermo_stop(struct cpufreq_policy *policy)
 {
-	struct thermo_cpuinfo *info = g_info;
-
-	del_timer_sync(&info->idle_timer);
-	del_timer_sync(&info->boost_timer);
+	del_timer_sync(&thermo_timer);
+	cancel_work_sync(&thermo_work);
 	input_unregister_handler(&thermo_input_handler);
-	kfree(info);
+	power_supply_unreg_notifier(&thermo_psy_notifier);
+	global_policy = NULL;
 }
 
-static struct cpufreq_governor cpufreq_gov_thermo_balance = {
-	.name = "thermo_balance",
+static struct cpufreq_governor thermo_gov = {
+	.name = GOVERNOR_NAME,
+	.owner = THIS_MODULE,
 	.init = thermo_start,
 	.exit = thermo_stop,
-	.owner = THIS_MODULE,
 };
 
-static int __init thermo_governor_init(void)
+static int __init thermo_gov_init(void)
 {
-	return cpufreq_register_governor(&cpufreq_gov_thermo_balance);
+	return cpufreq_register_governor(&thermo_gov);
 }
 
-static void __exit thermo_governor_exit(void)
+static void __exit thermo_gov_exit(void)
 {
-	cpufreq_unregister_governor(&cpufreq_gov_thermo_balance);
+	cpufreq_unregister_governor(&thermo_gov);
 }
+
+module_init(thermo_gov_init);
+module_exit(thermo_gov_exit);
 
 MODULE_AUTHOR("Jayzee");
-MODULE_DESCRIPTION("Thermo Balance CPUFreq Governor");
+MODULE_DESCRIPTION("Thermo Balance CPUFreq Governor - Minimal Lag & Heat");
 MODULE_LICENSE("GPL");
-
-fs_initcall(thermo_governor_init);
-module_exit(thermo_governor_exit);
